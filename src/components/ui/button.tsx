81758196f527569304018aaf56d60935bd49084b
Shaikh Ayaz <Sk.Ayaz2017@gmail.com>
2025-07-18 21:12:38 +0000

    here are some css values for the secondary button, lets see how it looks

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-gradient-to-b from-white to-[#F8F8F8] text-foreground shadow-[0px_0px_4px_0px_rgba(0,0,0,0.10)] hover:bg-accent/80",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
